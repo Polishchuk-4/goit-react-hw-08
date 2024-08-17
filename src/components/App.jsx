bf246474a91f6a0232ac08800cc09bbcc348298e
Denys Polishchuk <polischyk0000@gmail.com>
2024-08-17 20:07:28 +0300

    add persister

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,5 @@
 import { lazy, Suspense, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
-import { fetchContacts } from "../redux/contacts/operations.js";
-import { selectIsLoggedIn } from "../redux/auth/selectors.js";
 
 const Layout = lazy(() => import("./Layout/Layout.jsx"));
 const HomePage = lazy(() => import("./page/HomePage/HomePage.jsx"));
@@ -11,13 +8,6 @@ const RegisterPage = lazy(() => import("./page/RegisterPage/RegisterPage.jsx"));
 const ContactsPage = lazy(() => import("./page/ContactsPage/ContactsPage"));
 
 export default function App() {
-  const dispatch = useDispatch();
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
-  // useEffect(() => {
-  //   dispatch(fetchContacts());
-  // }, [isLoggedIn]);
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
